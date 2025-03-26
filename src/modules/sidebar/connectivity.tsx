5a1ea91e35487dc877a4422c31c2e225b4ec2aef
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-26 20:22:51 +1100

    sidebar: networks module

--- src/modules/sidebar/connectivity.tsx
+++ src/modules/sidebar/connectivity.tsx
@@ -1,7 +1,10 @@
 import Bluetooth from "./modules/bluetooth";
+import Networks from "./modules/networks";
 
 export default () => (
     <box vertical className="pane connectivity" name="connectivity">
+        <Networks />
+        <box className="separator" />
         <Bluetooth />
     </box>
 );
