6b0c9af07c6c8a08ac11522e2a8c7af60c210deb
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-27 16:45:25 +1100

    sidebar: streams module

--- src/modules/sidebar/audio.tsx
+++ src/modules/sidebar/audio.tsx
@@ -1,8 +1,10 @@
 import Media from "./modules/media";
+import Streams from "./modules/streams";
 
 export default () => (
     <box vertical className="pane audio" name="audio">
         <Media />
         <box className="separator" />
+        <Streams />
     </box>
 );
