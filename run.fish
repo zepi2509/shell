7c16c48b8c8f81b9e9f3959629483d4c934bd0a0
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-04-30 22:00:57 +1000

    dev: run script ignore process kill msg

--- run.fish
+++ run.fish
@@ -5,5 +5,6 @@ set -l hypr 'invalid nullptr parameter'  # Error that always pops up on Hyprland
 set -l intercept '^qsintercept:'  # Line before binding loop
 set -l loop 'Binding loop detected'  # Binding loops
 set -l icons 'Searching custom icon paths is not yet supported'  # Error for system tray icons
+set -l process 'QProcess: Destroyed while process'
 
-qs -c caelestia --log-rules $dbus | grep -v -e $hypr -e $intercept -e $loop -e $icons
+qs -c caelestia --log-rules $dbus | grep -v -e $hypr -e $intercept -e $loop -e $icons -e $process
