d0211b00dba0501c3b8aacc20367730d7da1152a
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-05-01 00:01:16 +1000

    dev: fix run script
    It was hiding other important qsintercepts, so make it only hide empty ones (i.e. ones with only file:line:column)

--- run.fish
+++ run.fish
@@ -2,9 +2,9 @@
 
 set -l dbus 'quickshell.dbus.properties.warning = false'  # System tray dbus property errors
 set -l hypr 'invalid nullptr parameter'  # Error that always pops up on Hyprland
-set -l intercept '^qsintercept:'  # Line before binding loop
+set -l intercept '^qsintercept:.*(:[0-9]+){2}$'  # Empty qsintercept lines
 set -l loop 'Binding loop detected'  # Binding loops
 set -l icons 'Searching custom icon paths is not yet supported'  # Error for system tray icons
 set -l process 'QProcess: Destroyed while process'
 
-qs -c caelestia --log-rules $dbus | grep -v -e $hypr -e $intercept -e $loop -e $icons -e $process
+qs -c caelestia --log-rules $dbus | grep -vE -e $hypr -e $intercept -e $loop -e $icons -e $process
