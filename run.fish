e3a1134804b1c42ec974aa53fcc6d271fbcd3b4e
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-05-24 14:46:36 +0800

    dev: update run script
    No need for many of those warnings anymore

--- run.fish
+++ run.fish
@@ -1,13 +1,8 @@
 #!/bin/fish
 
 set -l dbus 'quickshell.dbus.properties.warning = false'  # System tray dbus property errors
-set -l text_input 'qt.qpa.wayland.textinput.warning = false'  # Text input focus when open window
 set -l notifs 'quickshell.service.notifications.warning = false'  # Notification server warnings on reload
 set -l sni 'quickshell.service.sni.host.warning = false'  # StatusNotifierItem warnings on reload
-set -l hypr 'invalid nullptr parameter'  # Error that always pops up on Hyprland
-set -l intercept '^qsintercept:.*(:[0-9]+){2}$'  # Empty qsintercept lines
-set -l loop 'Binding loop detected'  # Binding loops
 set -l process 'QProcess: Destroyed while process'  # Long running processes on reload
-set -l async_loader 'items in the process of being created at engine destruction'  # Async loaders on reload
 
-qs -c caelestia --log-rules "$dbus;$text_input;$notifs;$sni" | grep -vE -e $hypr -e $intercept -e $loop -e $process -e $async_loader
+qs -c caelestia --log-rules "$dbus;$notifs;$sni" | grep -vE -e $process
