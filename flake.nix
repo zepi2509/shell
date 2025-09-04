58ff28cdf714d76dbe508ebf8eb795b159ea3c62
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-09-04 22:54:50 +1000

    nix: fix devshell stdenv
    Closes #560

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       default = let
         shell = self.packages.${pkgs.system}.caelestia-shell;
       in
-        pkgs.mkShell {
+        pkgs.mkShell.override {stdenv = shell.stdenv;} {
           inputsFrom = [shell shell.plugin shell.extras];
           packages = with pkgs; [material-symbols rubik nerd-fonts.caskaydia-cove];
           CAELESTIA_XKB_RULES_PATH = "${pkgs.xkeyboard-config}/share/xkeyboard-config-2/rules/base.lst";
