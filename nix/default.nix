48d93521489981dedf48518b0162dbac796fc95b
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-09-09 01:02:01 +1000

    nix: fix pipewire
    Fixes #583

--- nix/default.nix
+++ nix/default.nix
@@ -22,7 +22,7 @@
   quickshell,
   aubio,
   libcava,
-  libpipewire,
+  pipewire,
   xkeyboard-config,
   cmake,
   ninja,
@@ -86,7 +86,7 @@
     };
 
     nativeBuildInputs = [cmake ninja pkg-config];
-    buildInputs = [qt6.qtbase qt6.qtdeclarative qt6.qtmultimedia libqalculate libpipewire aubio libcava];
+    buildInputs = [qt6.qtbase qt6.qtdeclarative qt6.qtmultimedia libqalculate pipewire aubio libcava];
 
     dontWrapQtApps = true;
     cmakeFlags =
